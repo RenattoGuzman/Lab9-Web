4abf0177797d272345e0c45f44265c97d1a4e9cf
RenattoGuzman <renttogs@gmail.com>
2023-05-30 12:43:36 -0600

    Calculadora

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import React, { useEffect, useState } from 'react';
+import Screen from "./components/Screen";
+
+
+function App() {
+
+
+    return(
+        <div className="container"> 
+            <Screen/>       
+            <h1>hercules</h1>
+        </div>
+
+    ) 
+};
+
+export default App;
